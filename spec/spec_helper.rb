2a67b777d787174d41c765d4c70e248f3cdc33c9
Lucille Ilayka Apale <lucille.apale@appstone.tech>
2017-05-16 15:03:08 +0800

    Finish layout and routes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ Spork.prefork do
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
-  require 'rspec/autorun'
+  # require 'rspec/autorun'
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
@@ -43,6 +43,7 @@ Spork.prefork do
     #     --seed 1234
     config.order = "random"
     config.include Capybara::DSL
+    config.include Rails.application.routes.url_helpers
   end
 end
 
